ee881d60ea1608d083c833d8d5fe5e7290803fca
JeffSMLee <jeffreylee37@gmail.com>
2018-04-14 22:56:49 -0400

    Model baseline working.
    Pulls from deprecated data source but hey, if it works... remember to put
    tmp folder in gitignore, otherwise the training can get interrupted by git.

--- model.py
+++ model.py
@@ -6,24 +6,42 @@ import argparse
 import sys
 
 import tensorflow as tf
+import numpy as np
 import dataset
 
-feature_columns = [tf.feature_column.numeric_column("x", shape=[28, 28])]
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets('MNIST_data')
+
+feature_columns = [tf.feature_column.numeric_column("x", shape=[784])]
 classifier = tf.estimator.DNNClassifier(
         feature_columns=feature_columns,
         hidden_units=[64, 64],
+        n_classes=10,
         model_dir="./tmp/model",
-        n_classes=10
 )
 
-def train_input_fn():
-    ds = dataset.train("data")
-    ds = ds.cache().shuffle(buffer_size=50000).batch(100)
-    ds = ds.repeat(40)
-    return ds
-def eval_input_fn():
-    return dataset.test("data").batch(100).make_one_shot_iterator().get_next()
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": np.array(mnist.train.images)},
+      y=np.array(mnist.train.labels).astype(np.int32),
+      num_epochs=None,
+      shuffle=True)
+
+#def train_input_fn():
+    #ds = dataset.train("data")
+    #ds = ds.cache().shuffle(buffer_size=50000).batch(100)
+    #ds = ds.repeat(40)
+    #return ds
+
+#def eval_input_fn():
+#    return dataset.test("data").batch(100).make_one_shot_iterator().get_next()
+
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": np.array(mnist.test.images)},
+      y=np.array(mnist.test.labels).astype(np.int32),
+      num_epochs=1,
+      shuffle=False)
+
 
 classifier.train(input_fn=train_input_fn)
-eval_results = classifier.evaluate(input_fn=eval_input_fn)
-print("\nEvaluation results:\n\t%s\n" % eval_results)
+score = classifier.evaluate(input_fn=test_input_fn)['accuracy']
+print("\nTest Accuracy: {0:f}\n".format(score))
