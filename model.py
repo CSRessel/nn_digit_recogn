2e29aaf039fb3eb1a24670df9756eb5bb1d6e234
Clifford Ressel <cressel@andrew.cmu.edu>
2018-04-22 18:15:17 -0400

    time training

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import argparse
 import sys
+import time
 
 import tensorflow as tf
 import numpy as np
@@ -33,6 +34,7 @@ train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": np.array(mnist.train.images)},
       y=np.array(mnist.train.labels).astype(np.int32),
       num_epochs=None,
+	  batch_size=50,
       shuffle=True)
 
 test_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -43,6 +45,12 @@ test_input_fn = tf.estimator.inputs.numpy_input_fn(
 
 print("\nTraining classifier...\n")
 
-classifier.train(input_fn=train_input_fn)
+start = time.clock()
+classifier.train(input_fn=train_input_fn, steps=100000)
+finish = time.clock()
+
+wall_time = finish - start
+
 score = classifier.evaluate(input_fn=test_input_fn)['accuracy']
 print("\nTest Accuracy: {0:f}\n".format(score))
+print("\nTraining Time: {0:f}\n".format(wall_time))
