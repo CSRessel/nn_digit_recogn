853f3ca64f823b56e248ccbca4f95460e7d4e011
JeffSMLee <jeffreylee37@gmail.com>
2018-05-13 23:41:16 -0500

    updated cnn model with finalized hyperparams
    Run with two command line args. First for training label noise (float)
    second for testing data noise (float).

--- cnn.py
+++ cnn.py
@@ -1,72 +1,201 @@
-import numpy as np 
-import matplotlib as mp
-import matplotlib.pyplot as plt
+#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+"""Convolutional Neural Network Estimator for MNIST, built with tf.layers."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy as np
 import tensorflow as tf
-import tensorflow.contrib.slim as slim
-from tensorflow.examples.tutorials.mnist import input_data
-import math
+import sys
+import random
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+lnoises = sys.argv[1]
+lnoise = float(lnoises)
+snoise = float(sys.argv[2])
 
-tf.reset_default_graph()
-
-x = tf.placeholder(tf.float32, [None, 784],name="x-in")
-true_y = tf.placeholder(tf.float32, [None, 10],name="y-in")
-keep_prob = tf.placeholder("float")
-
-x_image = tf.reshape(x,[-1,28,28,1])
-hidden_1 = slim.conv2d(x_image,5,[5,5])
-pool_1 = slim.max_pool2d(hidden_1,[2,2])
-hidden_2 = slim.conv2d(pool_1,5,[5,5])
-pool_2 = slim.max_pool2d(hidden_2,[2,2])
-hidden_3 = slim.conv2d(pool_2,20,[5,5])
-hidden_3 = slim.dropout(hidden_3,keep_prob)
-out_y = slim.fully_connected(slim.flatten(hidden_3),10,activation_fn=tf.nn.softmax)
-
-cross_entropy = -tf.reduce_sum(true_y*tf.log(out_y))
-correct_prediction = tf.equal(tf.argmax(out_y,1), tf.argmax(true_y,1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+tf.logging.set_verbosity(tf.logging.INFO)
 
 
-batchSize = 50
-sess = tf.Session()
-init = tf.global_variables_initializer()
-sess.run(init)
-for i in range(1001):
-    batch = mnist.train.next_batch(batchSize)
-    sess.run(train_step, feed_dict={x:batch[0],true_y:batch[1], keep_prob:0.5})
-    if i % 100 == 0 and i != 0:
-        trainAccuracy = sess.run(accuracy, feed_dict={x:batch[0],true_y:batch[1], keep_prob:1.0})
-        print("step %d, training accuracy %g"%(i, trainAccuracy))
+def cnn_model_fn(features, labels, mode):
+  """Model function for CNN."""
+  # Input Layer
+  # Reshape X to 4-D tensor: [batch_size, width, height, channels]
+  # MNIST images are 28x28 pixels, and have one color channel
+  input_layer = tf.reshape(features["x"], [-1, 28, 28, 1])
+
+  # Convolutional Layer #1
+  # Computes 32 features using a 5x5 filter with ReLU activation.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 28, 28, 1]
+  # Output Tensor Shape: [batch_size, 28, 28, 32]
+  conv1 = tf.layers.conv2d(
+      inputs=input_layer,
+      filters=32,
+      kernel_size=5,
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #1
+  # First max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 28, 28, 32]
+  # Output Tensor Shape: [batch_size, 14, 14, 32]
+  pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+  # Convolutional Layer #2
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 14, 14, 32]
+  # Output Tensor Shape: [batch_size, 14, 14, 64]
+  conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=5,
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #2
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 14, 14, 64]
+  # Output Tensor Shape: [batch_size, 7, 7, 64]
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+# Convolutional Layer #2
+  # Computes 64 features using a 5x5 filter.
+  # Padding is added to preserve width and height.
+  # Input Tensor Shape: [batch_size, 14, 14, 32]
+  # Output Tensor Shape: [batch_size, 14, 14, 64]
+  conv3 = tf.layers.conv2d(
+      inputs=pool2,
+      filters=64,
+      kernel_size=5,
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #2
+  # Second max pooling layer with a 2x2 filter and stride of 2
+  # Input Tensor Shape: [batch_size, 14, 14, 64]
+  # Output Tensor Shape: [batch_size, 7, 7, 64]
+  pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
 
 
-testAccuracy = sess.run(accuracy, feed_dict={x:mnist.test.images,true_y:mnist.test.labels, keep_prob:1.0})
-print("test accuracy %g"%(testAccuracy))
+  # Flatten tensor into a batch of vectors
+  # Input Tensor Shape: [batch_size, 7, 7, 64]
+  # Output Tensor Shape: [batch_size, 7 * 7 * 64]
+  pool3_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+
+  # Dense Layer
+  # Densely connected layer with 1024 neurons
+  # Input Tensor Shape: [batch_size, 7 * 7 * 64]
+  # Output Tensor Shape: [batch_size, 1024]
+  dense = tf.layers.dense(inputs=pool3_flat, units=750, activation=tf.nn.relu)
+
+  # Logits layer
+  # Input Tensor Shape: [batch_size, 1024]
+  # Output Tensor Shape: [batch_size, 10]
+  logits = tf.layers.dense(inputs=dense, units=10)
+
+  predictions = {
+      # Generate predictions (for PREDICT and EVAL mode)
+      "classes": tf.argmax(input=logits, axis=1),
+      # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+      # `logging_hook`.
+      "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+  }
+  if mode == tf.estimator.ModeKeys.PREDICT:
+    return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+  # Calculate Loss (for both TRAIN and EVAL modes)
+  loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+  # Configure the Training Op (for TRAIN mode)
+  if mode == tf.estimator.ModeKeys.TRAIN:
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+    train_op = optimizer.minimize(
+        loss=loss,
+        global_step=tf.train.get_global_step())
+    return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+  # Add evaluation metrics (for EVAL mode)
+  eval_metric_ops = {
+      "accuracy": tf.metrics.accuracy(
+          labels=labels, predictions=predictions["classes"])}
+  return tf.estimator.EstimatorSpec(
+      mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-def getActivations(layer,stimuli):
-    units = sess.run(layer,feed_dict={x:np.reshape(stimuli,[1,784],order='F'),keep_prob:1.0})
-    plotNNFilter(units)
+def main(unused_argv):
+  # Load training and eval data
+  mnist = tf.contrib.learn.datasets.load_dataset("mnist")
+  train_data = mnist.train.images  # Returns np.array
+  train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+  eval_data = mnist.test.images  # Returns np.array
+  eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
-def plotNNFilter(units):
-    filters = units.shape[3]
-    plt.figure(1, figsize=(20,20))
-    n_columns = 6
-    n_rows = math.ceil(filters / n_columns) + 1
-    for i in range(filters):
-        plt.subplot(n_rows, n_columns, i+1)
-        plt.title('Filter ' + str(i))
-        plt.imshow(units[0,:,:,i], interpolation="nearest", cmap="gray")
+  num_samples = train_labels.shape[0]
+  num_noisy_labels = num_samples * lnoise
+  garbled_labels = random.sample(range(num_samples), int(num_noisy_labels))
+  for i in garbled_labels:
+    inc_labels = [np.int32(i) for i in range(10)]
+    inc_labels.remove(train_labels[i])
+    train_labels[i] = random.choice(inc_labels)
+
+  def noisify(x, w):
+      rand = np.random.rand(*x.shape)
+      return x * (1. - w) + rand * w
+
+  noisy_test_samples = np.zeros(eval_data.shape)
+  for i in range(eval_data.shape[0]):
+    noisy_test_samples[i] = noisify(eval_data[i], snoise)
+
+  cfg = tf.estimator.RunConfig(
+          save_checkpoints_secs = None,
+          keep_checkpoint_max = None,
+  )
+  
+  # Create the Estimator
+  mnist_classifier = tf.estimator.Estimator(
+      model_fn=cnn_model_fn, model_dir="convnet_model"+lnoises)#, config=cfg)
+
+  # Set up logging for predictions
+  # Log the values in the "Softmax" tensor with label "probabilities"
+  tensors_to_log = {"probabilities": "softmax_tensor"}
+  logging_hook = tf.train.LoggingTensorHook(
+      tensors=tensors_to_log, every_n_iter=50)
+
+  # Train the model
+  train_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": train_data},
+      y=train_labels,
+      batch_size=100,
+      num_epochs=None,
+      shuffle=True)
+  mnist_classifier.train(
+      input_fn=train_input_fn,
+      steps=10000,
+      hooks=[logging_hook])
+
+  # Evaluate the model and print results
+  eval_input_fn = tf.estimator.inputs.numpy_input_fn(
+      x={"x": noisy_test_samples},
+      y=eval_labels,
+      num_epochs=1,
+      shuffle=False)
+  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  print(eval_results)
 
 
-imageToUse = mnist.test.images[0]
-plt.imshow(np.reshape(imageToUse,[28,28]), interpolation="nearest", cmap="gray")
-plt.show()
-
-getActivations(hidden_1,imageToUse)
-plt.show()
-getActivations(hidden_2,imageToUse)
-plt.show()
-getActivations(hidden_3,imageToUse)
-plt.show()
+if __name__ == "__main__":
+  tf.app.run()
